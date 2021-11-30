82b6f3f4f1c438e9aeeeff87e1432f354e8ee7c1
Riccardo Mammina <riccardo.mammina@gmail.com>
2021-11-30 19:07:10 +0100

    completato esercizio 9.1

--- Es-Angular2-part2/src/app/services/counter.service.ts
+++ Es-Angular2-part2/src/app/services/counter.service.ts
@@ -3,4 +3,26 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class CounterService {}
+export class CounterService {
+  private counter:number = 0;
+
+  constructor() { }
+
+  getValue(): number {
+    return this.counter;
+  }
+
+  increseCounter(value: number = 1): number {
+    return this.counter + value;
+  }
+
+  decreseCounter(value: number = 1): number {
+    return this.counter - value;
+  }
+
+  errorCounter() {
+    if (this.counter < 0) {
+      console.error('Counter non può essere negativo')
+    }
+  }
+}
